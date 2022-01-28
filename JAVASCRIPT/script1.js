0a7e7b64506031e4917b1c2d27be044d9d8c53bd
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-28 20:11:13 +0530

    Javascript Basics

---
+++ JAVASCRIPT/script1.js
@@ -0,0 +1,14 @@
+console.time('Your code took');
+console.log("Hello World");
+console.log(45+45);
+console.log(true);
+console.log([1,2,3,4,5,6]);
+console.log({marks:90,name:"omkar"});
+console.table({marks:90,name:"omkar"});
+console.warn("This is Warning");
+// console.clear();
+console.timeEnd('Your Code took');
+// console.assert(150 > 189), "This is not Possible";
+console.error("This is an Error");
+// Single line Comment
+// Multiline comment same as java
