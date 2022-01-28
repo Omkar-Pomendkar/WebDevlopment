f75e595712b2f19e9a7b855c1481e3bf7026ede1
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-28 21:19:59 +0530

    VAriables in Javascript

---
+++ JAVASCRIPT/Variables.js
@@ -0,0 +1,19 @@
+// Variable var let const 
+console.log("Varaibles in js");
+var names = "Omkar";
+var channel;
+// channel = "OSP";
+var marks = 34;
+console.log(marks);
+console.log(names,channel);
+// Rules for Creating Javscript Variables
+// Same as another Language
+//Modern Javascript does not uses var Keyword
+// const is constant variable
+// const ownerName = "omkar";
+// ownername = "Dinesh"; cannot do this
+console.log(ownername);
+// let Keyword
+
+    let phone = "Samsung";
+    console.log(phone);
