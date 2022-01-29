9b692ae2216d45f9f457adc18470a71daa8c0661
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-29 10:51:31 +0530

    Structured in Folder

---
+++ JAVASCRIPT/typeConversion.js
@@ -0,0 +1,10 @@
+//JavaScript Type Conversion & Coercion
+console.log("Welcome to Javascript");
+let myVar = 34;
+console.log(myVar,typeof myVar);
+myVar = String(34);
+console.log(myVar,typeof myVar);
+
+arr = [1,2,3,4,5,6];
+arr = String(arr);
+console.log(arr, typeof arr);
