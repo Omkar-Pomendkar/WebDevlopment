8f3a5ab9ab7b537b80c91617eb3000b22f2a27db
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-29 12:16:41 +0530

    Type Conversion

---
+++ JAVASCRIPT/04_TypeConversion/typeConversion.js
@@ -0,0 +1,23 @@
+//JavaScript Type Conversion & Coercion
+console.log("Welcome to Javascript");
+let myVar = 34;
+console.log(myVar,typeof myVar);
+myVar = String(34);
+console.log(myVar,typeof myVar);
+
+arr = [1,2,3,4,5,6];
+arr = String(arr);
+console.log(arr, typeof arr);
+
+let i = 56;
+console.log(i.toString);
+
+let number = parseInt("45");
+// let number = parseFloat("45");
+console.log(number, typeof(number));
+
+//Type Coercoin
+let myStr = Number("567");
+let myNum = 67;
+console.log((myStr+myNum));
+console.log
