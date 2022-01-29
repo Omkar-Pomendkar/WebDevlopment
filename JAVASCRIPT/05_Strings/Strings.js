bf638ad90c8de557bf05c2309be6a59f48d4e723
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-29 12:52:43 +0530

    Strings

---
+++ JAVASCRIPT/05_Strings/Strings.js
@@ -0,0 +1,19 @@
+// Strings
+const name = "Omkar";
+const greeting = "Good Morning";
+console.log(name + ' '+greeting);
+let html;
+html = "<h1>This is Heading</h1>";
+// html = html.concat("HEllo");
+// console.log(html);
+// console.log(html.toLowerCase());
+// console.log(html.toUpperCase());
+console.log(html.indexOf("h1"));
+console.log(html.lastIndexOf("h1"));
+console.log(html.slice("h1"));
+console.log(html.charAt(5));
+console.log(html.endsWith('hello'));
+console.log(html.includes('Heading'));
+console.log(html.substring(1,7));
+
+
