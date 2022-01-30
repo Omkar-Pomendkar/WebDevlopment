d20259557e7d2b222bd1ab1eb75598efd623f3f7
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-30 12:13:47 +0530

    Document Object Model

---
+++ JAVASCRIPT/11_Website_Layout/dom.js
@@ -0,0 +1,7 @@
+console.log("Understanding DOM & Creating a Website Layout ");
+let a = document;
+a = document.forms[0];
+a = document.links[0];
+a = document.images;
+a = document.script;
+console.log(a);
