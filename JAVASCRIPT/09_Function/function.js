8e64f1592c8671776cd36e96ffb68ab3a1e48957
Omkar Pomendkar <65494978+Omkar-Pomendkar@users.noreply.github.com>
2022-01-30 11:14:14 +0530

    Functions

---
+++ JAVASCRIPT/09_Function/function.js
@@ -0,0 +1,29 @@
+var i = 34;
+console.log(i);
+
+
+function name(params) {
+    
+    return
+}
+
+// function greet(names,thanks) {
+//     console.log(`Hello ${names} Everybody I am practicing Javasacript ${thanks}`);
+// }
+
+
+
+
+// console.log("Functions");
+// let names = "omkar";
+// let thanks = "Thanks a lot";
+// // let names1 = "Rohan";
+// greet(names,thanks);
+// // console.log(`Hello ${names} Everybody I am practicing Javasacript`);
+// // console.log(`Hello ${names1} Everybody I am practicing Javasacript`);
+
+// arr = ["orange","banana", "kiwi"];
+// arr.forEach(function(element,index,array){
+//     console.log(element,index)
+
+// });
